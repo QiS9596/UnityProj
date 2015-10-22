586cae179df2f9b07fb99e409c6bfb6f56d1614b
Rolo <rolo_lamperouge@foxmail.com>
2015-10-22 12:35:35 +0800

    project 1 tank war first day tests

---
+++ Proj1-TankWar/Tankwar/Assets/_Script/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public GameObject Explosion;
+	public AudioClip sExplosion;
+	public Material material;
+
+
+	void OnTriggerEnter(Collider other){
+		Destroy (this.gameObject);
+		GameObject NewExplosion = (GameObject)Instantiate (Explosion, transform.position, Quaternion.identity);
+//		switch (other.tag) {
+//
+//		case Tags.Enemy:
+//			other.gameObject.GetComponent<Enemy>().attacked();
+//			break;
+//		case Tags.Player:
+//			other.gameObject.GetComponent<Player>().attacked();
+//			break;
+//		}
+	}
+	// Use this for initialization
+	void Start () {
+//		TagManager = new Tags();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Proj1-TankWar/Tankwar/Assets/_Script/Tags.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Tags {
+
+
+
+	public const string Barrel = "Barrel";
+	public const string Enemy = "Enemy";
+	public const string Player = "Player";
+	public const string GameController = "GameController";
+}

7ab98a4fb2ad7665e53dd43c9f3852792673fd89
Rolo <rolo_lamperouge@foxmail.com>
2015-10-23 02:05:55 +0800

    update
    update relevant control system

--- Proj1-TankWar/Tankwar/Assets/_Script/PlayerController.cs
+++ Proj1-TankWar/Tankwar/Assets/_Script/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 	public float backwordSpeed = -10.0f;
 	public float smooth = 0.5f;
 	public float rotateSpeed = 1.0f;
+	public float forwardSpeedMax = 20.0f;
+	public float backwardSpeedMax = 10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,16 +16,32 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed);
+		/*moving forward and backward*/
+		gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3(0,-10,0));
+		if (Input.GetKey (KeyCode.UpArrow) ) {
+			gameObject.GetComponent<Rigidbody>().velocity = transform.right * speed;
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed * -1.0f);
 		}
-
+		/*rotate the whole tank*/
 		float h = Input.GetAxis ("Horizontal");
 		transform.Rotate(0, h * rotateSpeed, 0);
 
+
+	}
+
+	void ButtonDebugTest(){
+		if (Input.GetButton ("Fire1")) {
+			Debug.Log("Fire1");
+		}
+		if (Input.GetButton ("Fire2"))
+			Debug.Log ("Fire2");
+		if (Input.GetButton ("Fire3"))
+			Debug.Log ("Fire3");
+		if (Input.GetButton ("Jump"))
+			Debug.Log ("Jump");
+		if (Input.GetButton ("Submit"))
+			Debug.Log ("Submit");
 	}
 }

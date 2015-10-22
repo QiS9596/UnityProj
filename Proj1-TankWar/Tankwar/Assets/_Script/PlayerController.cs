ba35c455ea53e7dd444aa80cb06feea68c68f781
Rolo <rolo_lamperouge@foxmail.com>
2015-10-22 21:28:50 +0800

    control system of tank war
    update basic control system of tank, including moving forward, backward
    and rotation

---
+++ Proj1-TankWar/Tankwar/Assets/_Script/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 60.0f;
+	public float backwordSpeed = -10.0f;
+	public float smooth = 0.5f;
+	public float rotateSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed * -1.0f);
+		}
+
+		float h = Input.GetAxis ("Horizontal");
+		transform.Rotate(0, h * rotateSpeed, 0);
+
+	}
+}
